2e564b70f9cdf2a27d7770cca5ab6b13f63decd3
unknown <wanghuasheng45@163.com>
2025-07-08 16:36:38 +0800

    初学者开发三步骤学习

--- src/main/java/com/wanghuasheng/chapter0/Test.java
+++ src/main/java/com/wanghuasheng/chapter0/Test.java
@@ -1,4 +1,4 @@
-package com.wanghuasheng.chapter1;
+package com.wanghuasheng.chapter0;
 
 public class Test {
     public static void main(String[] args) {
---
+++ src/main/java/com/wanghuasheng/chapter0/example/Helloworld.java
@@ -0,0 +1,5 @@
+public class Helloworld{
+    public static void main(String[] args) {
+        System.out.println("这是我第一java代码");
+    }
+}
